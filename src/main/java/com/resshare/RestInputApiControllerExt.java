4c805f9638a3a34ab91ee3dc3097bea59cc168ba
hoangdung1008@gmail.com <hoangdung1008@gmail.com>
2023-01-29 09:13:11 +0700

    first commit

--- src/main/java/com/resshare/RestInputApiControllerExt.java
+++ src/main/java/com/resshare/RestInputApiControllerExt.java
@@ -1,28 +1,161 @@
 package com.resshare;
 
-import java.net.URI;
-import java.util.Map;
+//import java.net.URI;
+//import java.util.Map;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.util.UriComponentsBuilder;
+//
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.gson.Gson;
+//import com.resshare.framework.core.controller.RestInputApiController;
+//import com.resshare.framework.core.service.ResFirebaseReference;
+//import com.resshare.framework.model.Input;
+//import com.resshare.framework.model.Output;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class RestInputApiControllerExt extends RestInputApiController {}
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.gson.Gson;
-import com.resshare.framework.core.controller.RestInputApiController;
-import com.resshare.framework.core.service.ResFirebaseReference;
-import com.resshare.framework.model.Input;
-import com.resshare.framework.model.Output;
+//package com.resshare.framework.core.controller;
+
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.gson.Gson;
+        import com.resshare.framework.core.service.ResFirebaseReference;
+        import com.resshare.framework.model.Input;
+        import com.resshare.framework.model.Output;
+        import java.net.URI;
+        import java.util.Map;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.http.HttpHeaders;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.RequestBody;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.client.RestTemplate;
+        import org.springframework.web.util.UriComponentsBuilder;
+
 
 @RestController
 @RequestMapping("/api")
-public class RestInputApiControllerExt extends RestInputApiController {}
+        public class RestInputApiControllerExt {
+    public static final Logger logger = LoggerFactory.getLogger(com.resshare.framework.core.controller.RestInputApiController.class);
+    public static String REST_SERVICE_URI = "http://localhost:8088/api";
+
+    public RestInputApiControllerExt() {
+    }
+
+    @RequestMapping(
+            value = {"/input/"},
+            method = {RequestMethod.POST}
+    )
+    public ResponseEntity<?> request(@RequestBody Input input, UriComponentsBuilder ucBuilder) {
+        logger.info("POST input : {}", input);
+        if (this.validate(input)) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+            try {
+                Gson gson = new Gson();
+                Map map = input.getJsonmap();
+                if (input.getJson() != null) {
+                    map = (Map)gson.fromJson(input.getJson(), Map.class);
+                }
+
+                String sKey = null;
+                String dataCollection = input.getDataCollection();
+                if ("draft/core/flowchart_execute".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                if ("draft/core/seach_app".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                if ("draft/core/get_home_layout".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                if ("draft/core/get_home_default_layout".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                if ("draft/core/get_layout_data".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                if ("draft/core/get_data".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                if ("draft/core/get_main_dashboard_app".equals(dataCollection)) {
+                    dataCollection = "../" + dataCollection;
+                }
+
+                String path_load_form_script = ResFirebaseReference.getInputPathReference(dataCollection);
+                DatabaseReference reference = database.getReference(path_load_form_script);
+                if (map != null) {
+                    if (map.containsKey("key_use_user_id") && map.containsKey("user_id") && map.get("key_use_user_id").equals(true)) {
+                        sKey = (String)map.get("user_id");
+                    }
+
+                    if (sKey != null) {
+                        reference.child(sKey).setValue(map);
+                    } else if (input.getId() != null) {
+                        reference.child(input.getId()).setValue(map);
+                    } else {
+                        reference.push().setValue(map);
+                    }
+                }
+            } catch (Exception var10) {
+                var10.printStackTrace();
+            }
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/api/input/{aplication}").buildAndExpand(new Object[]{input.getApplication()}).toUri());
+        return new ResponseEntity(headers, HttpStatus.OK);
+    }
+
+    private boolean validate(Input input) {
+        String dataCollection = input.getDataCollection();
+        return dataCollection != null;
+    }
+
+    private static void postOutput(Output output) {
+        System.out.println("Testing postOutput Output API----------");
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = restTemplate.postForLocation(REST_SERVICE_URI + "/output/", output, new Object[]{Output.class});
+        System.out.println("Location : " + uri.toASCIIString());
+    }
+
+    @RequestMapping(
+            value = {"/output/"},
+            method = {RequestMethod.POST}
+    )
+    public ResponseEntity<?> response(@RequestBody Output output, UriComponentsBuilder ucBuilder) {
+        logger.info("POST Output : {}", output);
+        postOutput(output);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/api/output/{aplication}").buildAndExpand(new Object[]{output.getAplication()}).toUri());
+        return new ResponseEntity(headers, HttpStatus.OK);
+    }
+}
